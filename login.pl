8f1beda0f157760b76a58c3541bc28438f26cad3
aamz <alex.marenco@gmail.com>
2013-07-04 09:08:06 -0600

    Update login.pl

--- login.pl
+++ login.pl
@@ -1,8 +1,31 @@
 use Mojolicious::Lite;
-
-use lib 'lib';
-use MyUsers;
 use Mojo::IOLoop;
+
+{
+package MyUsers;
+
+use strict;
+use warnings;
+
+my $USERS = {
+  'dinko.iliev'    => '1234',
+  'sergio.caballero' => '1234',
+  'rodolfo.estrada'    => '1234',
+  'agustin.romero'    => '1234'
+};
+
+sub new { bless {}, shift }
+
+sub check {
+  my ($self, $user, $pass) = @_;
+
+  # Success
+  return 1 if $USERS->{$user} && $USERS->{$user} eq $pass;
+
+  # Fail
+  return undef;
+}	
+}
 
 app->secret('Mojolicious rocks and Alex rocks');
 
